6be4485ca513a75ef02dd5cb8746402de5e03beb
PanruifengWawa <46937922@qq.com>
2016-09-22 20:10:12 +0800

    login

---
+++ js/indexController.js
@@ -0,0 +1,34 @@
+myApp.controller("indexController",function($scope,$http) {
+	$scope.logined = 0;
+	$scope.login = function() {
+		$scope.userName = document.getElementById("userName");
+		$scope.password = document.getElementById("password");
+		if($scope.userName == undefined || $scope.userName =="") {
+			alert("请输入用户名");
+			return;
+		}
+		if($scope.password == undefined || $scope.password =="") {
+			alert("请输入密码");
+			return;
+		}
+		var formData = new FormData();
+	    formData.append('userName', userName);
+	    formData.append("password",password);
+		$http({
+			url:'http://182.92.226.65:8080/GXBike/api/user/login',
+			method:'POST',
+			headers: {
+				'Content-Type': undefined
+			},
+			data:formData
+		}).success(function(data,header,config,status){
+			if(data.data == null) {
+				alert("登录失败");
+			} else {
+				
+			}
+		}).error(function(data,header,config,status){
+			
+		});
+	}
+})
